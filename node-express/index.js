4bbb2518c879c90c332bc664e191d03384cda13f
Amirreza <aazam.amirreza@gmail.com>
2022-12-22 13:32:05 -0500

    implmenting rest API using express

--- node-express/index.js
+++ node-express/index.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const http = require('http');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const bodyParser = require ('body-parser')
 
 const hostname = 'localhost';
 const port = 3000;
@@ -8,6 +9,32 @@ const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes', (req,res,next) => {
+   res.statusCode = 200;
+   res.setHeader('Content-Type', 'text/html');
+   next(); 
+});
+app.get('/dishes/:dishId', (req,res,next) =>{
+    res.end ('Will send '+ req.params.dishId + ' to you.');
+});
+app.get('/dishes', (req,res,next) =>{
+    res.end ('Will send all dishes to uuu');
+});
+
+app.post ('/dishes', (req,res,next) => {
+    res.end ('will add your retarded looking dishes to the database ' + req.body.name + 'with details' + req.body.description);
+});
+
+app.put ('/dishes', (req,res,next) => {
+    res.statusCode = 403;
+    res.end ('PUT not supported pn /dishes bro');
+});
+
+app.delete ('/dishes', (req,res,next) => {
+    res.end ('deleting all the dishes. yess alll of them');
+});
 
 app.use(express.static(__dirname + '/public'));
 
