8c1c45c2655d358b5b0bd62248ad71907146e826
Amirreza Aazam <aazam.amirreza@gmail.com>
2022-12-26 23:43:13 -0500

    connected mongodb using mongoose

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -10,6 +10,17 @@ const dishRouter = require ('./routes/dishRouter');
 const promoRouter = require ('./routes/promoRouter');
 const leaderRouter = require ('./routes/leaderRouter');
 
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://127.0.0.1:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log("Connected correctly to daaaaa server");
+}, (err) => { console.log(err); });
+
 var app = express();
 
 // view engine setup
--- conFusionServer/routes/dishRouter.js
+++ conFusionServer/routes/dishRouter.js
@@ -1,63 +1,78 @@
-// THIS IS THE ROUTER FOR /DISHES. If nothing has been attached, we will go with the first route but if its /dishes/something, then we will go for the specific route defined.
-
 const express = require('express');
-const bodyParser = require ('body-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Dishes = require('../models/dishes');
 
 const dishRouter = express.Router();
 
 dishRouter.use(bodyParser.json());
 
-dishRouter.route ('/')
-.all( (req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    next(); 
- })
- .get( (req,res,next) =>{
-     res.end ('Will send all dishes to uuu');
- })
-.post ( (req,res,next) => {
-     res.end ('will add your retarded looking dishes to the database ' + req.body.name + 'with details' + req.body.description);
- }) 
-.put ( (req,res,next) => {
-     res.statusCode = 403;
-     res.end ('PUT not supported pn /dishes bro');
- })
-.delete ( (req,res,next) => {
-     res.end ('deleting all the dishes. yess alll of them');
- });
+dishRouter.route('/')
+.get((req,res,next) => {
+    Dishes.find({})
+    .then((dishes) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dishes);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post((req, res, next) => {
+    Dishes.create(req.body)
+    .then((dish) => {
+        console.log('Dish Created ', dish);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dishes');
+})
+.delete((req, res, next) => {
+    Dishes.remove({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));    
+});
 
+dishRouter.route('/:dishId')
+.get((req,res,next) => {
+    Dishes.findById(req.params.dishId)
+    .then((dish) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dishes/'+ req.params.dishId);
+})
+.put((req, res, next) => {
+    Dishes.findByIdAndUpdate(req.params.dishId, {$set: req.body}, { new: true })
+    .then((dish) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.delete((req, res, next) => {
+    Dishes.findByIdAndRemove(req.params.dishId)
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
 
- dishRouter.route ('/:dishId')
-.all( (req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    next(); 
- })
- .get( (req,res,next) =>{
-     res.end ('Will send ' + req.params.dishId +' dish to uuu');
- })
-.post ( (req,res,next) => {
-     res.end ('will add your retarded looking dishes (' + req.params.dishId + ') to the database ' + req.body.name + 'with details' + req.body.description);
- }) 
-.put ( (req,res,next) => {
-     res.statusCode = 403;
-     res.end ('PUT not supported pn /dishes bro');
- })
-.delete ( (req,res,next) => {
-     res.end ('deleting all the dishes. yess alll of them');
- });
-
-//extra dish router in case user inputs two / after dishes.
-
- dishRouter.route ('/:dishId/:dishNum')
- .all( (req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    next(); 
- })
- .get( (req,res,next) =>{
-     res.end ('Will send ' + req.params.dishId +' dish to uuu with name ' + req.params.dishNum);
- });
-
- module.exports = dishRouter;
+module.exports = dishRouter;
